633aec75e853d78568c060a615412b7078417668
andrew-whites <andrewwhites1995@gmail.com>
2018-08-10 22:37:22 +0100

    Initial commit

---
+++ src/main/java/com/andrew/white/nihonguNotification/NihonguNotificationApplication.java
@@ -0,0 +1,12 @@
+package com.andrew.white.nihonguNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NihonguNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NihonguNotificationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/andrew/white/nihonguNotification/NihonguNotificationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andrew.white.nihonguNotification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NihonguNotificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
